8fed805514b3f97a2de8fd3b6b86832069bb272d
Ellie Crome <ecrome@andigital.com>
2018-04-10 12:04:22 +0100

    added redux to handle ticks

--- src/components/App/app.jsx
+++ src/components/App/app.jsx
@@ -1,29 +1,32 @@
 import React, { Component } from "react";
-import Shapes from "../Shapes";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 import Square from "../Square";
 
-var data = [
-  new Shapes.O(1, 1),
-  new Shapes.I(1, 4),
-  new Shapes.T(1, 9),
-  new Shapes.S(1, 13),
-  new Shapes.Z(1, 17),
-  new Shapes.L(1, 21),
-  new Shapes.J(1, 25)
-];
+const mapStateToProps = state => {
+  return {
+    shapes: state
+  };
+};
 
 class App extends Component {
   render() {
     return (
       <div>
-        {data.map(shape =>
+        {this.props.shapes.map(shape =>
           shape
             .squares()
-            .map(sq => <Square key={data.index} row={sq.row} col={sq.col} />)
+            .map(sq => (
+              <Square key={sq.row + " " + sq.col} row={sq.row} col={sq.col} />
+            ))
         )}
       </div>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  shapes: PropTypes.array
+};
+
+export default connect(mapStateToProps)(App);
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 import { render } from "react-dom";
-
-// main app
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import App from "./components/App";
+import Shapes from "./components/Shapes";
 
-render(<App />, document.getElementById("app"));
+let initialState = [
+  new Shapes.O(1, 1),
+  new Shapes.I(1, 4),
+  new Shapes.T(1, 9),
+  new Shapes.S(1, 13),
+  new Shapes.Z(1, 17),
+  new Shapes.L(1, 21),
+  new Shapes.J(1, 25)
+];
+
+const store = createStore(reducer);
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "TICK":
+      return [...state, new Shapes.O(action.data * 2, action.data * 2)];
+    default:
+      return state;
+  }
+}
+
+var counter = 1;
+setInterval(() => {
+  store.dispatch({ type: "TICK", data: counter++ });
+}, 1000);
+
+render(
+  <Provider store={store}>
+    <App shapes={initialState} />
+  </Provider>,
+  document.getElementById("app")
+);
